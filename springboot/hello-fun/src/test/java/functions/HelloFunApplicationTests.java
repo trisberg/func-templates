ff1b2375935242974891bf75ed9c969c732c1d00
Thomas Risberg <trisberg@pivotal.io>
2022-10-30 17:28:59 -0400

    Update templates, add hello-fun

---
+++ springboot/hello-fun/src/test/java/functions/HelloFunApplicationTests.java
@@ -0,0 +1,13 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
